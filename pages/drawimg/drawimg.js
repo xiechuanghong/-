0e12af6dfc9b30f582fc3eefeb4d9b53a587fb1b
lrm <963256712@qq.com>
2017-12-12 18:35:44 +0800

    推广二维码

--- pages/drawimg/drawimg.js
+++ pages/drawimg/drawimg.js
@@ -4,7 +4,6 @@
  */
 const app = getApp();
 const config = require("../../utils/config.js");
-const ctx = wx.createCanvasContext('can');
 Page({
 
   /**
@@ -12,7 +11,8 @@ Page({
    */
   data: {
     dprHeight: 1,
-    dprWidth: 1
+    dprWidth: 1,
+    canId: 'can'
   },
 
   /**
@@ -22,8 +22,8 @@ Page({
     console.log(app.globalData);
     console.log(app.globalData.bg_cover);
     console.log(app.globalData.qrcode);
-    console.log(ctx);
-    var _this = this;
+    var _this = this,
+        ctx = wx.createCanvasContext('can');
     wx.getSystemInfo({
       success: function(res) {
         _this.setData({
@@ -32,19 +32,24 @@ Page({
         })
         ctx.drawImage(app.globalData.bg_cover, 0, 0, res.windowWidth, res.windowHeight);
         ctx.drawImage(app.globalData.qrcode, (res.windowWidth - 92 * _this.data.dprWidth) / 2, 160 , 92 * _this.data.dprWidth, 92 * _this.data.dprWidth);
+        ctx.draw(true);
       },
     });
     ctx.setFontSize(24 * _this.data.dprWidth);
     ctx.setFillStyle('#FFFFFF');   
     ctx.fillText(app.globalData.nickName, 140 * _this.data.dprWidth, 50 * _this.data.dprHeight);
+    ctx.draw(true)
     ctx.setFontSize(14 * _this.data.dprWidth);
     ctx.fillText('我为' + app.globalData.shop.shop_name + '小程序代言', 140 * _this.data.dprWidth, 80 * _this.data.dprHeight);
+    ctx.draw(true)
     ctx.arc(90, 55, 30, 0, 2 * Math.PI)
     ctx.clip()
     ctx.drawImage(app.globalData.avatarImg, 60, 25, 60, 60);
-    ctx.restore();
-    
-    ctx.draw();
+
+    ctx.draw(true);
+    setTimeout(function(){
+      _this.aaa()
+    },200)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -52,7 +57,6 @@ Page({
   onReady: function () {
 
   },
-
   /**
    * 生命周期函数--监听页面显示
    */
@@ -73,7 +77,39 @@ Page({
   onUnload: function () {
 
   },
+  aaa: function() {
+    var that = this;
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      destWidth: 750,
+      destHeight: 1206,
+      canvasId: 'can',
+      success: function (res) {
+        console.log(res.tempFilePath)
 
+        that.setData({
+          url: res.tempFilePath
+        })
+
+        wx.previewImage({
+          current: res.tempFilePath, // 当前显示图片的http链接
+          urls: [res.tempFilePath], // 需要预览的图片http链接列表
+          success:()=>{
+            that.setData({
+              img:'display:none'
+            })
+          }
+        })
+      
+      },
+      complete:(res)=>{
+        console.log(2222)
+        console.log(res)
+
+      }
+    });
+  },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
